fdd9b3e763f8c3c899ba9e23a6369302fbf8ebc6
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-08-03 01:45:14 +0000

    created debugger ruby file

--- howdy.rb
+++ howdy.rb
@@ -1,6 +1,18 @@
 # here's a comment
+
 require "active_support/all"
+
+pp "What's your name?"
+#their_name = gets
+their_name = gets.chomp
+
+#pp "Hello, " + their_name + "!"
+puts "Hello, " + their_name + "!"
+
+=begin
 my_string = "Hello, world!"
+require "./goodbye.rb"
+
 pp 1.ordinalize    # => "1st"
 pp 2.ordinalize    # => "2nd"
 pp 53.ordinalize   # => "53rd"
@@ -16,5 +28,4 @@ pp "equipment".pluralize # => "equipment"
 pp "tables".singularize    # => "table"
 pp "rubies".singularize    # => "ruby"
 pp "equipment".singularize # => "equipment"
-
-require "./goodbye.rb"
+=end
