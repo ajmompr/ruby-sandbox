5a7095d67a7bc6c59cc93df86bc1034fc63e8dbc
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-08-05 19:55:56 +0000

    updated encoded_msg variable & conditions

--- secret_encoder.rb
+++ secret_encoder.rb
@@ -5,9 +5,27 @@ secret = [
 ].sample
 secret = secret.downcase
 split_secret = secret.split("")
+encoded_msg = []
 pp secret
 pp split_secret
 # write your program below
 =begin
 secret code: a = 1, e = 2, i = 3, o = 4, u = 5
 =end
+split_secret.each do |letter|
+  if letter == "a"
+    encoded_msg.push("1")
+  elsif letter == "e"
+    encoded_msg.push("2")
+  elsif letter == "i"
+    encoded_msg.push(3)
+  elsif letter == "o"
+    encoded_msg.push(4)
+  elsif letter == "u"
+    encoded_msg.push("5")
+  else
+    encoded_msg.push(letter)
+  end
+end
+
+pp encoded_msg
