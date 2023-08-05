41cb4fdcf391bb29ac20906f1e2c55b8d9a887c7
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-08-05 19:33:13 +0000

    added split_secret variable

---
+++ secret_encoder.rb
@@ -0,0 +1,12 @@
+secret = [
+  "I have a secret to share",
+  "Is this secure enough for my PASSWORD?",
+  "we should be more clever"
+].sample
+split_secret = secret.split("")
+pp secret
+pp split_secret
+# write your program below
+=begin
+secret code: a = 1, e = 2, i = 3, o = 4, u = 5
+=end
