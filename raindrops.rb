75afff4f073c9157ffd3930538ab05bdf24b1a37
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-08-05 21:52:39 +0000

    updated final if statement

--- raindrops.rb
+++ raindrops.rb
@@ -1,13 +1,35 @@
+
 integers = [1, 21, 35, 105]
 integer = integers.sample
+
 # write your program below
+# integer = 21
 raindrops = ""
-if integer % 3 == 0
-  pp "Pling"
-elsif integer % 5 == 0
-  pp "Plang"
-elsif integer % 7 == 0
-  pp "Plong"
-else
-  pp integer
-end
+
+  
+   if integer % 3 == 0 && integer % 5 == 0 && integer % 7 == 0
+      raindrops += "Pling"
+      raindrops += "Plang"
+      raindrops += "Plong"
+      pp raindrops
+    elsif integer % 3 == 0 && integer % 5 == 0 
+      raindrops += "Pling"
+      raindrops += "Plang"
+      pp raindrops
+    elsif   integer % 3 == 0 && integer % 7 == 0
+      raindrops += "Pling"
+      raindrops += "Plong"
+      pp raindrops
+    elsif integer % 5 == 0 && integer % 7 == 0
+      raindrops += "Plang"
+      raindrops += "Plong"
+      pp raindrops
+    elsif integer % 5 == 0 
+      raindrops += "Plang"
+      pp raindrops
+    elsif integer % 7 == 0
+      raindrops += "Plong"
+      pp raindrops  
+    else
+      pp integer
+    end
