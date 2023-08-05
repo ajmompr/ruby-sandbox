78df6c27fdbbe71111e090e32526e69c5d861139
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-08-05 01:28:12 +0000

    updated typo in pp statement

--- diceroll.rb
+++ diceroll.rb
@@ -2,7 +2,7 @@ dice_roll = rand(1..6)
 guess_num = rand(1..6)
 
 if dice_roll == guess_num
-  pp "You guesed correctly."
+  pp "You guessed correctly."
 else 
   pp "Sorry, you guessed #{guess_num}. The die landed on #{dice_roll}."
 end
