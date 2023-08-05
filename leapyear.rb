1b02e29e24735c532122977d44a3784f060ac9fe
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-08-05 01:42:09 +0000

    added else condition for not a leap year

--- leapyear.rb
+++ leapyear.rb
@@ -1,2 +1,15 @@
 require "date"
 
+years = [
+  1700,
+  1940,
+  2038
+]
+
+year = years.sample
+# write your program below
+if year % 4 == 0 && year % 100 == 0 && year % 400 == 0
+  pp "#{year} is a leap year!"
+else
+  pp "#{year} is not a leap year."
+end
