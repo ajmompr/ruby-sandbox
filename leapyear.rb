2fddc16e780590c8b4e1398701f6128c451683b3
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-08-05 02:27:38 +0000

    corrected bad math and debugged program

--- leapyear.rb
+++ leapyear.rb
@@ -6,8 +6,12 @@ years = [
 
 year = years.sample
 # write your program below
-if year % 4 == 0 || year % 100 == 0 || year % 400 == 0
-  pp "#{year} is a leap year!"
+
+if year % 4 != 0
+  pp "#{year} is not a leap year."
+elsif year % 100 == 0 && year % 400 != 0
+  pp "#{year} is not a leap year."
 else
- pp "#{year} is not a leap year." 
+  pp "#{year} is a leap year!" 
 end
+  
