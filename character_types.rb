d2e6209e29bde3eabce7c19751c475724a1975fe
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-08-07 01:52:04 +0000

    updated regexes

--- character_types.rb
+++ character_types.rb
@@ -6,5 +6,20 @@ strings = [
 string = strings.sample
 pp string
 # write your program below
-char_array = string.split("")
-pp char_array
+letter_count = 0
+number_count = 0
+space_count = 0
+letter_string = string.gsub(/[^a-z]/i, "")
+number_string = string.gsub(/[^0-9]/, "")
+
+
+#pp letter_string
+#pp number_string
+
+letter_array = letter_string.split("")
+number_array = number_string.split("")
+space_array = string.split("")
+
+pp letter_array
+pp number_array
+pp space_array
