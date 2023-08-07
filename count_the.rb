76969ad111043e4220e16f29fe696fa6f5f74e19
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-08-07 03:08:33 +0000

    added pp statement

--- count_the.rb
+++ count_the.rb
@@ -3,8 +3,19 @@ sentences = [
   "the, the code, and the developer",
   "then the- their"
 ]
-#sentence = sentences.sample
-# write your program below
-
-
+sentence = sentences.sample
+# write your program
+the_count = 0
 sub_sentence = sentence.gsub(/\bthe\b/i, "*")
+#pp sub_sentence
+
+sentence_array = sub_sentence.split("")
+#pp sentence_array
+
+sentence_array.each do |character|
+  if character == "*"
+    the_count += 1
+  end
+end
+
+pp "'the' appeared #{the_count.to_s} times"
