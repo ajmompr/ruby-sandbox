b68ea68b92bf5491b54e0bdd6c1cefc34e49bce2
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-08-07 01:53:10 +0000

    created letter, number, and space count variables

--- character_types.rb
+++ character_types.rb
@@ -6,20 +6,20 @@ strings = [
 string = strings.sample
 pp string
 # write your program below
-letter_count = 0
-number_count = 0
-space_count = 0
 letter_string = string.gsub(/[^a-z]/i, "")
 number_string = string.gsub(/[^0-9]/, "")
-
-
 #pp letter_string
 #pp number_string
-
 letter_array = letter_string.split("")
 number_array = number_string.split("")
 space_array = string.split("")
 
+letter_count = 0
+number_count = 0
+space_count = 0
+
 pp letter_array
 pp number_array
 pp space_array
+
+letter_array.each do |letter|
